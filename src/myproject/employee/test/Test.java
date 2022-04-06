2cd98125cced4a9665ed5338cbb818c3287231de
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-04-06 23:49:15 +0530

    Hibernate load method

--- src/myproject/employee/test/Test.java
+++ src/myproject/employee/test/Test.java
@@ -18,11 +18,23 @@ public class Test {
 		configuration.configure("myproject/employee/resources/hibernate.cfg.xml");
 		sessionFactory = configuration.buildSessionFactory();
 		session = sessionFactory.openSession();
-		Employee employee = (Employee)session.get("myproject.employee.pojo.Employee", 8);
+		/*Employee employee = (Employee)session.load("myproject.employee.pojo.Employee", 80);
+		if(employee == null) {
+			System.out.println("Employee does not existed");
+		}else {
 		System.out.println(employee.getEno());
 		System.out.println(employee.getEname());
 		System.out.println(employee.getEsal());
 		System.out.println(employee.getEaddr());
+		}*/
+		System.out.println("Before Calling Load method");
+		Employee employee = (Employee) session.load(Employee.class, 8);
+		System.out.println("After Calling Load Method");
+//		System.out.println(employee.getEno());
+//		System.out.println(employee.getEname());
+//		System.out.println(employee.getEsal());
+//		System.out.println(employee.getEaddr());
+		
 		}catch (Exception e) {
 			e.printStackTrace();
 		}finally {
